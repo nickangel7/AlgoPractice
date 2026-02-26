05aeb4b6eef63e534d73ec874b8d2b3cfb14949d
Nicholas Angelopoulos <nicholasangel1357@gmail.com>
2026-02-26 09:47:13 -0500

    binary tree practice

---
+++ invertBinaryTree.js
@@ -0,0 +1,15 @@
+class TreeNode {
+    constructor(val = 0, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class Solution {
+
+    invertTree(root){}
+}
+
+
